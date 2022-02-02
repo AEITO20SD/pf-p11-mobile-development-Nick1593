9a9c50cdbe2840100f34bd25b432c7b85c062759
Nick1593 <71027024+Nick1593@users.noreply.github.com>
2022-02-02 13:06:23 +0100

    Hello World Button
    Displays "Hello World"

---
+++ Hello World/Hello World/Hello World/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Hello_World
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void Button_Clicked(object sender, System.EventArgs e)
+        {
+            Hello.Text = $"Hello World";
+        }
+    }
+}
