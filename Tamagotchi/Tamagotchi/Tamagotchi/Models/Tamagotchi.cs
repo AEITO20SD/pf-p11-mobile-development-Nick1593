302ad43634011dc4a78858188be1a8b066f77388
Nick1593 <71027024+Nick1593@users.noreply.github.com>
2022-03-15 09:59:57 +0100

    Tamagotchi model created
    Created a tamagotchi model with some proporties

---
+++ Tamagotchi/Tamagotchi/Tamagotchi/Models/Tamagotchi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tamagotchi.Models
+{
+    internal class Tamagotchi
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Attention { get; set; }
+        public int Nutrition { get; set; }
+        public int Sleep { get; set; }
+        public DateTime Birth { get; set; }
+        public DateTime Death { get; set; }
+
+    }
+}
