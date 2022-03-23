2a1dbc89e023b13ba9555e4f7fa78e11bdbc4af4
Nick1593 <71027024+Nick1593@users.noreply.github.com>
2022-03-23 14:15:23 +0100

    Added DbContext
    Added the DbContext

---
+++ Tamagotchi/Tamagotchi/Tamagotchi/Services/TamagotchiContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Xamarin.Essentials;
+using Tamagotchi.Models;
+
+namespace Tamagotchi.Services
+{
+    public class TamagotchiContext : DbContext
+    {
+        private readonly string _databasename = "tamagotchi.db";
+        private readonly List<string> _tags = new List<string> { "Happy", "Neutral", "Sad", "Sleep", "Dead", "Egg_Fase_1", "Egg_Fase_2", "Egg_Fase_3", "Egg_Fase_4" };
+        public DbSet<Pet> Pets { get; set; }
+        public DbSet<PetType> PetTypes { get; set; }
+        public DbSet<ImageType> ImageTypes { get; set; }
+
+        public TamagotchiContext()
+        {
+            // Needed for IOS
+            /*SQLitePCL.Batteries_V2.Init();*/
+
+            this.Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string dbpath = Path.Combine(FileSystem.AppDataDirectory, _databasename);
+
+            optionsBuilder.UseSqlite($"Filename={dbpath}");
+        }
+    }
+}
