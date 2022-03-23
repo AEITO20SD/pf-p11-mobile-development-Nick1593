3788b2023dbf267d5370e5fc129a4a36088834b1
Nick1593 <71027024+Nick1593@users.noreply.github.com>
2022-03-23 14:16:46 +0100

    Added Console App
    Seperate console app for use of migrations. This cause xamarin doesn't work with EFCore wich runs on .net 5.0

---
+++ Tamagotchi/XamarinMigration/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace XamarinMigration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
