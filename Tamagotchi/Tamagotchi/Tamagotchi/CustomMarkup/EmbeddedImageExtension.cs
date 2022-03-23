6ae7855866795e284cf629f03720cb5f96c57c67
Nick1593 <71027024+Nick1593@users.noreply.github.com>
2022-03-23 14:19:23 +0100

    Added Class EmbeddedImageExtention class
    Added for use of embedded images from the main xamarin project

---
+++ Tamagotchi/Tamagotchi/Tamagotchi/CustomMarkup/EmbeddedImageExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tamagotchi.CustomMarkup
+{
+    [ContentProperty(nameof(Source))]
+    public class EmbeddedImageExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+            {
+                return null;
+            }
+
+            var imageSource = ImageSource.FromResource(Source, typeof(EmbeddedImageExtension).GetTypeInfo().Assembly);
+
+            return imageSource;
+        }
+    }
+}
