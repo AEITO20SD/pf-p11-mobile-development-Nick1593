5eb7be8581bc2d340243b510a59eec046dd737c8
Nick1593 <71027024+Nick1593@users.noreply.github.com>
2022-03-23 20:02:02 +0100

    Seeding changes
    Changed seeding to the right URL/Source (added Tamagotchi.)

--- Tamagotchi/Tamagotchi/Tamagotchi/Services/TamagotchiContext.cs
+++ Tamagotchi/Tamagotchi/Tamagotchi/Services/TamagotchiContext.cs
@@ -42,15 +42,15 @@ namespace Tamagotchi.Services
             // Basic images
             List<ImageType> ImageTypeList_Basic = new List<ImageType>();
 
-            ImageTypeList_Basic.Add(new ImageType { Id = 1, PetTypeId = PetTypeList[0].Id, Tag = _tags[0], URL = "images.pets.basic.tamagotchi_happy.png" });
-            ImageTypeList_Basic.Add(new ImageType { Id = 2, PetTypeId = PetTypeList[0].Id, Tag = _tags[1], URL = "images.pets.basic.tamagotchi_normal.png" });
-            ImageTypeList_Basic.Add(new ImageType { Id = 3, PetTypeId = PetTypeList[0].Id, Tag = _tags[2], URL = "images.pets.basic.tamagotchi_sad.png" });
-            ImageTypeList_Basic.Add(new ImageType { Id = 4, PetTypeId = PetTypeList[0].Id, Tag = _tags[3], URL = "images.pets.basic.tamagotchi_sleep.png" });
-            ImageTypeList_Basic.Add(new ImageType { Id = 5, PetTypeId = PetTypeList[0].Id, Tag = _tags[4], URL = "images.pets.basic.tamagotchi_dead.png" });
-            ImageTypeList_Basic.Add(new ImageType { Id = 6, PetTypeId = PetTypeList[0].Id, Tag = _tags[5], URL = "images.eggs.egg_fase_1.png" });
-            ImageTypeList_Basic.Add(new ImageType { Id = 7, PetTypeId = PetTypeList[0].Id, Tag = _tags[6], URL = "images.eggs.egg_fase_2.png" });
-            ImageTypeList_Basic.Add(new ImageType { Id = 8, PetTypeId = PetTypeList[0].Id, Tag = _tags[7], URL = "images.eggs.egg_fase_3.png" });
-            ImageTypeList_Basic.Add(new ImageType { Id = 9, PetTypeId = PetTypeList[0].Id, Tag = _tags[8], URL = "images.eggs.egg_fase_4.png" });
+            ImageTypeList_Basic.Add(new ImageType { Id = 1, PetTypeId = PetTypeList[0].Id, Tag = _tags[0], URL = "Tamagotchi.images.pets.basic.tamagotchi_happy.png" });
+            ImageTypeList_Basic.Add(new ImageType { Id = 2, PetTypeId = PetTypeList[0].Id, Tag = _tags[1], URL = "Tamagotchi.images.pets.basic.tamagotchi_normal.png" });
+            ImageTypeList_Basic.Add(new ImageType { Id = 3, PetTypeId = PetTypeList[0].Id, Tag = _tags[2], URL = "Tamagotchi.images.pets.basic.tamagotchi_sad.png" });
+            ImageTypeList_Basic.Add(new ImageType { Id = 4, PetTypeId = PetTypeList[0].Id, Tag = _tags[3], URL = "Tamagotchi.images.pets.basic.tamagotchi_sleep.png" });
+            ImageTypeList_Basic.Add(new ImageType { Id = 5, PetTypeId = PetTypeList[0].Id, Tag = _tags[4], URL = "Tamagotchi.images.pets.basic.tamagotchi_dead.png" });
+            ImageTypeList_Basic.Add(new ImageType { Id = 6, PetTypeId = PetTypeList[0].Id, Tag = _tags[5], URL = "Tamagotchi.images.eggs.egg_fase_1.png" });
+            ImageTypeList_Basic.Add(new ImageType { Id = 7, PetTypeId = PetTypeList[0].Id, Tag = _tags[6], URL = "Tamagotchi.images.eggs.egg_fase_2.png" });
+            ImageTypeList_Basic.Add(new ImageType { Id = 8, PetTypeId = PetTypeList[0].Id, Tag = _tags[7], URL = "Tamagotchi.images.eggs.egg_fase_3.png" });
+            ImageTypeList_Basic.Add(new ImageType { Id = 9, PetTypeId = PetTypeList[0].Id, Tag = _tags[8], URL = "Tamagotchi.images.eggs.egg_fase_4.png" });
 
             modelBuilder.Entity<ImageType>().HasData(
                 ImageTypeList_Basic
