a8279460cfe91b8db7233b3b95993a0fd868d016
Nick1593 <71027024+Nick1593@users.noreply.github.com>
2022-03-23 14:11:45 +0100

    Added Class ImageType
    Added ImageType class as model

---
+++ Tamagotchi/Tamagotchi/Tamagotchi/Models/ImageType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tamagotchi.Models
+{
+    public class ImageType
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MaxLength(200)]
+        public string Tag { get; set; }
+        [Required]
+        public string URL { get; set; }
+        public int PetTypeId { get; set; }
+        public virtual PetType PetType { get; set; }
+    }
+}
