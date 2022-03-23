fa205a7f8b6f4dee74882cf7c748463f88e81ec2
Nick1593 <71027024+Nick1593@users.noreply.github.com>
2022-03-23 20:30:34 +0100

    Added INotifyPropertyChanged
    Added INotifyPropertyChanged as a class and made a second class which is to inherit the first to be able to bind commands

---
+++ Tamagotchi/Tamagotchi/Tamagotchi/Services/BaseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Tamagotchi.Services
+{
+    public abstract class BaseViewModel : OnPropertyChanged
+    {
+        public Dictionary<string, ICommand> Commands { get; protected set; }
+        public BaseViewModel()
+        {
+            Commands = new Dictionary<string, ICommand>();
+        }
+    }
+}
---
+++ Tamagotchi/Tamagotchi/Tamagotchi/Services/OnPropertyChanged.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Tamagotchi.Services
+{
+    public class OnPropertyChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
