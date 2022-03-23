ef11f3d6dcd5610bf0b97f0805c96acfc6530ea7
Nick1593 <71027024+Nick1593@users.noreply.github.com>
2022-03-23 14:11:04 +0100

    Added PetType Class
    Added PetType class as model. It has a relation with Pet & TypeImage.

---
+++ Tamagotchi/Tamagotchi/Tamagotchi/Models/PetType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tamagotchi.Models
+{
+    public class PetType
+    {
+        public PetType()
+        {
+            Pets = new HashSet<Pet>();
+            TypeImages = new HashSet<ImageType>();
+        }
+        [Key]
+        public int Id { get; set; }
+        [Required, MaxLength(100)]
+        public string TypeName { get; set; }
+        public virtual ICollection<Pet> Pets { get; set; }
+        public virtual ICollection<ImageType> TypeImages { get; set; }
+    }
+}
