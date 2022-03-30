c30383e1f3b81f3fb95e0df6a5d6d3ff5ced3fd0
Nick1593 <71027024+Nick1593@users.noreply.github.com>
2022-03-30 14:19:25 +0200

    Moved Baseviewmodel
    Moved to ViewModel map

--- Tamagotchi/Tamagotchi/Tamagotchi/ViewModels/BaseViewModel.cs
+++ Tamagotchi/Tamagotchi/Tamagotchi/ViewModels/BaseViewModel.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Windows.Input;
+using Tamagotchi.Services;
 
-namespace Tamagotchi.Services
+namespace Tamagotchi.ViewModels
 {
     // Baseviewmodel to allow commands to be binded
-    public abstract class BaseViewModel : OnPropertyChanged
+    public abstract class BaseViewModel : NotifyPropertyChanged
     {
         public Dictionary<string, ICommand> Commands { get; protected set; }
         public BaseViewModel()
