e1b563019328efda48aa656c8eb40ee31526a789
Nick1593 <71027024+Nick1593@users.noreply.github.com>
2022-04-12 15:13:56 +0200

    Added listview to AddPet for all pettypes

--- Tamagotchi/Tamagotchi/Tamagotchi/View/AddPet.xaml.cs
+++ Tamagotchi/Tamagotchi/Tamagotchi/View/AddPet.xaml.cs
@@ -1,20 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Tamagotchi.Services;
+using Tamagotchi.ViewModels;
 
 namespace Tamagotchi.View
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AddPet : ContentPage
     {
-        public AddPet()
+        private AddPetViewModel _viewModel;
+        public AddPet(TamagotchiService service)
         {
             InitializeComponent();
+            _viewModel = new AddPetViewModel(service);
+            BindingContext = _viewModel;
         }
     }
 }
